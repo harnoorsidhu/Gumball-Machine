acb09acffa9f66e223384753f1d40e78c2cd0e90
Harnoor Sidhu <76535441+harnoorsidhu@users.noreply.github.com>
2021-12-26 16:25:55 -0600

    Add files via upload

---
+++ State.java
@@ -0,0 +1,14 @@
+package State;
+
+/** From Head First Design Patterns, Freeman & Robson et al. */  
+
+public interface State {
+
+    public void insertQuarter();
+
+    public void ejectQuarter();
+
+    public void turnCrank();
+
+    public void dispense();
+}
